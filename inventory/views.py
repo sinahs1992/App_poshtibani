02716895e93367441bc5bfd798c6422a37c962e0
sinahs1992 <sinahs1992@gmail.com>
2024-01-07 21:49:09 +0330

    Opt Out

--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from inventory.models import Device
 from django.db.models import Q
 from jdatetime import datetime
+from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required, permission_required
 
@@ -11,10 +12,24 @@ def index(request):
 @login_required
 @permission_required('accounts.view_staff_page', raise_exception=True)
 def staff_page(request):
+    try:
+        rows = Device.objects.filter(Q(recepted=None) & Q(installed=None), staff=request.user).order_by('-created_date')
+        now = datetime.now()
+        context = {'rows':rows, 'now':now}
+        if request.method == "POST":
+            staff = request.user.staff
+            print(staff.staff)
+            sn = request.POST.get('sn')
+            if staff and sn:
+                Device.objects.create(staff=staff, device_code=sn, status='O')
+                messages.success(request, 'دستگاه نصبی با موفقیت ثبت شد.')
+                return render(request, 'management/inventory.html', context)
+            else:
+                messages.error(request, 'لطفاً تمام فیلدها را پر کنید.')
 
-    rows = Device.objects.filter(Q(recepted=None) & Q(installed=None), staff=request.user).order_by('-created_date')
-    now = datetime.now()
-    context = {'rows':rows, 'now':now}
-    return render(request, 'inventory/job.html', context)
 
+        return render(request, 'inventory/job.html', context)
+    except PermissionError:
+        messages.error(request, "You do not have permission to view this page.")
+        return redirect('inventory:index')
 
